8e23c5107dfd169d8978e3954d83e401187c1327
nquynhhhhhhh <phamnhuquynh13122002@gmail.com>
2025-12-02 01:13:04 +0700

    bài 11

--- src/test/java/BT1/AddCategory.java
+++ src/test/java/BT1/AddCategory.java
@@ -3,33 +3,80 @@ package BT1;
 import Common.BaseTest;
 import Common.Locators;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
-import java.time.Duration;
+import java.util.List;
 
 public class AddCategory extends BaseTest {
 
-    @Test (priority = 1)
-    public void menuDashboard() throws InterruptedException {
-        driver.findElement(By.xpath(Locators.inputSearchMenuDashboard)).sendKeys("Category");
-        driver.findElement(By.xpath(Locators.menuCategory)).click();
+    @Test
+    public void dashboardPage() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Assert.assertEquals(driver.findElement(By.xpath(Locators.tabDashboard)).getText(), "Dashboard", "Tab title does not match");
+        softAssert.assertEquals(driver.findElement(By.xpath(Locators.tabOrders)).getText(), "Orders", "Tab title does not match");
         Thread.sleep(1000);
-        System.out.println("Header Category: " + driver.findElement(By.xpath(Locators.headerCategoryPage)).getText());
-        driver.findElement(By.xpath(Locators.inputSearchCategory)).sendKeys("My bear");
-        System.out.println("Item: " + driver.findElement(By.xpath(Locators.itemMyBear)).getText());
+        softAssert.assertEquals(driver.findElement(By.xpath(Locators.buttonAddNewDashboard)).getText(), "Add New", "Button does not match");
+        Assert.assertEquals(driver.findElement(By.xpath(Locators.userNameDashboard)).getText(), "Admin Example", "Username does not match");
+
+        String dashboardColor = driver.findElement(By.xpath(Locators.tabDashboard)).getCssValue("color");
+        String ordersColor = driver.findElement(By.xpath(Locators.tabOrders)).getCssValue("color");
+        Assert.assertNotEquals(dashboardColor, ordersColor, "Dashboard and Orders must have different colors");
+        softAssert.assertAll();
     }
 
-    @Test (priority = 2)
+    @Test
+    public void categoryPage() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        driver.findElement(By.xpath(Locators.inputSearchMenuDashboard)).sendKeys("Category");
+        driver.findElement(By.xpath(Locators.menuCategory)).click();
+        Thread.sleep(2000);
+        Assert.assertEquals(driver.findElement(By.xpath(Locators.headerCategoryPage)).getText(), "All categories", "Header does not match");
+        driver.findElement(By.xpath(Locators.inputSearchCategory)).sendKeys("My bear");
+        driver.findElement(By.xpath(Locators.inputSearchCategory)).submit();
+        Thread.sleep(1000); //submit gây ra việc reload lại trang => cần thêm time để load
+        System.out.println("Item: " + driver.findElement(By.xpath(Locators.itemMyBear)).getText());
+        List<WebElement> headerTable = driver.findElements(By.xpath("//tr[@class='footable-header']//th[contains(@style, 'table-cell')]"));
+        System.out.println("Header Name of table: " + headerTable.size());
+        for (int i = 0; i<headerTable.size(); i++){
+            System.out.println(headerTable.get(i).getText());
+        }
+
+        Thread.sleep(2000);
+        List<WebElement> itemMyBear = driver.findElements(By.xpath("//tbody//td[contains(@style, 'table-cell')]"));
+        softAssert.assertEquals(itemMyBear.get(1).getText(), "My bear", "Name does not match");
+        softAssert.assertEquals(itemMyBear.get(2).getText(), "Kids & Toy", "Parent Category does not match");
+        softAssert.assertEquals(itemMyBear.get(3).getText(), "1", "Order Level does not match");
+        softAssert.assertEquals(itemMyBear.get(4).getText(), "1", "Level does not match");
+        softAssert.assertEquals(itemMyBear.get(6).getText(), "—","Icon does not null");
+
+        //muốn check isSeclect thì phải có thẻ input => kh dùng List do chỉ bắt đến thẻ td
+        Thread.sleep(2000);
+        boolean itemFeatured = driver.findElement(By.xpath(Locators.toggleFeaturedFirst)).isSelected(); //TH này dùng kiểm tra ON/ OFF, enable là xem có thao tác đc trên đó ktra có bị vô hiệu hóa hay không
+        softAssert.assertFalse(itemFeatured,  "Featured is ON");
+        //điền biến boolean itemFeatured vào => kiểm tra giá trị hiện tại của toggle
+        boolean itemHotCategory = driver.findElement(By.xpath(Locators.toggleHotCategory)).isSelected();
+        softAssert.assertTrue(itemHotCategory, "Hot Category is OFF");
+
+
+        softAssert.assertEquals(itemMyBear.get(10).getText(), "", "");
+
+        Thread.sleep(2000);
+
+
+        softAssert.assertAll();
+    }
+
+    @Test
     public void dashboardPOS() throws InterruptedException {
         driver.findElement(By.xpath(Locators.menuPOS)).click();
         driver.findElement(By.xpath("//span[normalize-space()='POS Manager']")).click();
         Thread.sleep(1000);
     }
+
 }
 
 
--- src/test/java/Common/Locators.java
+++ src/test/java/Common/Locators.java
@@ -2,7 +2,6 @@ package Common;
 
 public class Locators {
     //Login page
-    public static String headerLogin = "//h1[normalize-space()='Welcome to eCommerce CMS']";
     public static String inputEmail = "//input[@id='email']";
     public static String inputPassword = "//input[@id='password']";
     public static String buttonLogin = "//button[normalize-space()='Login']";
@@ -11,6 +10,11 @@ public class Locators {
     public static String alertErrorMessage = "//span[normalize-space()='Invalid login credentials']";
 
     //Dashboard page
+    public static String tabDashboard = "//a[normalize-space()='Orders']/preceding-sibling::a";
+    public static String tabOrders = "//a[normalize-space()='Orders']";
+    public static String buttonAddNewDashboard = "//span[normalize-space()='Add New']/parent::span";
+    public static String userNameDashboard = "//a[@data-toggle='dropdown']//span[normalize-space()='Admin Example']";
+
     public static String inputSearchMenuDashboard = "//input[@id='menu-search']";
     public static String menuPOS = "//span[normalize-space()='POS System']";
     public static String menuProducts = "//span[normalize-space()='Products']";
@@ -18,12 +22,20 @@ public class Locators {
     public static String menuNotes = "//span[normalize-space()='Notes']";
     public static String menuCategory = "//ul[@id='search-menu']//span[normalize-space()='Category']";
 
-    //Category
+    //Category //h5[normalize-space()='Categories']
     public static String headerCategoryPage = "//h1[normalize-space()='All categories']";
+    public static String headerTableCategories = "//h5[normalize-space()='Categories']";
     public static String buttonAddNewCategory = "//span[normalize-space()='Add New category']";
     public static String inputSearchCategory = "//input[@id='search']";
     public static String itemMyBear = "//td[normalize-space()='My bear']";
-
+    public static String toggleFeaturedFirst = "(//label[@class='aiz-switch aiz-switch-success mb-0']/input[@onchange='update_featured(this)'])[1]";
+    public static String toggleHotCategory= "(//label[@class='aiz-switch aiz-switch-success mb-0']/input[@onchange='update_hot(this)'])[1]";
+    public static String buttonDelete= "//a[@title='Delete'][1]";
+    public static String buttonEdit= "//a[@title='Edit'][1]";
+    public static String titleDialog= "//h4[normalize-space()='Delete Confirmation']";
+    public static String buttonCancelDialog= "//button[normalize-space()='Cancel']";
+    public static String buttonDeleteDialog= "//a[normalize-space()='Delete']";
+    public static String textDialog= "//p[normalize-space()='Are you sure to delete this?']";
 
     //Add New Category
     public static String headerAddNewCategory = "//h5[normalize-space()='Category Information']";
