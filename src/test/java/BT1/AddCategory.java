d1f265c623bdd1fc9bb8382cda28ec489b6ec42b
nquynhhhhhhh <phamnhuquynh13122002@gmail.com>
2025-12-04 01:33:51 +0700

    BT3

--- src/test/java/BT1/AddCategory.java
+++ src/test/java/BT1/AddCategory.java
@@ -4,7 +4,6 @@ import Common.BaseTest;
 import Common.Locators;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WindowType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -40,34 +39,83 @@ public class AddCategory extends BaseTest {
         Thread.sleep(1000); //submit gây ra việc reload lại trang => cần thêm time để load
         System.out.println("Item: " + driver.findElement(By.xpath(Locators.itemMyBear)).getText());
         List<WebElement> headerTable = driver.findElements(By.xpath("//tr[@class='footable-header']//th[contains(@style, 'table-cell')]"));
-        System.out.println("Header Name of table: " + headerTable.size());
-        for (int i = 0; i<headerTable.size(); i++){
+        System.out.println("Header Name of table: " + headerTable.size() + " items");
+        for (int i = 0; i < headerTable.size(); i++) {
             System.out.println(headerTable.get(i).getText());
         }
 
+        System.out.println("===========");
+
         Thread.sleep(2000);
-        List<WebElement> itemMyBear = driver.findElements(By.xpath("//tbody//td[contains(@style, 'table-cell')]"));
-        softAssert.assertEquals(itemMyBear.get(1).getText(), "My bear", "Name does not match");
-        softAssert.assertEquals(itemMyBear.get(2).getText(), "Kids & Toy", "Parent Category does not match");
-        softAssert.assertEquals(itemMyBear.get(3).getText(), "1", "Order Level does not match");
-        softAssert.assertEquals(itemMyBear.get(4).getText(), "1", "Level does not match");
-        softAssert.assertEquals(itemMyBear.get(6).getText(), "—","Icon does not null");
+        List<WebElement> itemMyBear = driver.findElements(By.xpath("//tbody//td[normalize-space()='My bear' and @class='d-flex align-items-center']/following-sibling::td[contains(@style, 'table-cell')]"));
+        Assert.assertEquals(driver.findElement(By.xpath(Locators.itemMyBear)).getText(), "My bear", "Name does not match");
+        Thread.sleep(1000);
+        //xpath từ My bear => sau My Bear là 0
+        softAssert.assertEquals(itemMyBear.get(0).getText(), "Kids & Toy", "Parent Category does not match");
+        softAssert.assertEquals(itemMyBear.get(1).getText(), "1", "Order Level does not match");
+        softAssert.assertEquals(itemMyBear.get(2).getText(), "1", "Level does not match");
+        //thuộc tính src thuộc thẻ <img> nhưng itemMyBear chỉ lấy đến thẻ <td> do đó phải .findElement(By.tagName("img"))
+        softAssert.assertEquals(itemMyBear.get(3).findElement(By.tagName("img")).getAttribute("src"), "https://ecommerce.anhtester.com/public/uploads/all/cSoDjVnjVAQwb1lMbm5vd3kHCSaq2nRwKnW6Adwh.webp", "Image Banner does not match");
+        softAssert.assertEquals(itemMyBear.get(4).getText(), "—", "Icon does not null");
+        softAssert.assertEquals(itemMyBear.get(5).findElement(By.tagName("img")).getAttribute("src"), "https://ecommerce.anhtester.com/public/uploads/all/cSoDjVnjVAQwb1lMbm5vd3kHCSaq2nRwKnW6Adwh.webp", "Cover Image does not match");
 
         //muốn check isSeclect thì phải có thẻ input => kh dùng List do chỉ bắt đến thẻ td
-        Thread.sleep(2000);
-        boolean itemFeatured = driver.findElement(By.xpath(Locators.toggleFeaturedFirst)).isSelected(); //TH này dùng kiểm tra ON/ OFF, enable là xem có thao tác đc trên đó ktra có bị vô hiệu hóa hay không
-        softAssert.assertFalse(itemFeatured,  "Featured is ON");
+        boolean itemFeatured = driver.findElement(By.xpath(Locators.toggleFeatured)).isSelected(); //chú ý xpath, toggle Featured là vị trí số 7
+        softAssert.assertFalse(itemFeatured, "Featured is ON");
+        if (itemFeatured == false) {
+            driver.findElement(By.xpath(Locators.toggleFeaturedClick)).click();
+            Thread.sleep(1000);
+            softAssert.assertEquals(driver.findElement(By.xpath(Locators.dialogFeatured)).getText(), "Featured categories updated successfully", "Dialog does not match");
+            boolean itemFeatured2 = driver.findElement(By.xpath(Locators.toggleFeatured)).isSelected(); //cập nhật sau khi click
+            softAssert.assertTrue(itemFeatured2, "Featured is OFF");
+            Thread.sleep(1000);
+            driver.findElement(By.xpath(Locators.toggleFeaturedClick)).click();
+        }
+
+        Thread.sleep(1000);
         //điền biến boolean itemFeatured vào => kiểm tra giá trị hiện tại của toggle
         boolean itemHotCategory = driver.findElement(By.xpath(Locators.toggleHotCategory)).isSelected();
         softAssert.assertTrue(itemHotCategory, "Hot Category is OFF");
-
-
-        softAssert.assertEquals(itemMyBear.get(10).getText(), "", "");
+        if (itemHotCategory == true) {
+            driver.findElement(By.xpath(Locators.toggleHotCategoryClick)).click();
+            Thread.sleep(1000);
+            softAssert.assertEquals(driver.findElement(By.xpath(Locators.dialogHotCategory)).getText(), "Hot categories updated successfully", "Dialog does not match");
+            boolean itemHotCategory2 = driver.findElement(By.xpath(Locators.toggleHotCategory)).isSelected();
+            softAssert.assertFalse(itemHotCategory2, "Hot Category is ON");
+            Thread.sleep(1000);
+            driver.findElement(By.xpath(Locators.toggleHotCategoryClick)).click();
+        }
 
         Thread.sleep(2000);
+        WebElement optionsDelete = driver.findElement(By.xpath(Locators.buttonDelete));
+        softAssert.assertEquals(optionsDelete.getAttribute("title"), "Delete", "Button Delete does not match");
+        driver.findElement(By.xpath(Locators.buttonDelete)).click();
+        Thread.sleep(1000);
+        Assert.assertEquals(driver.findElement(By.xpath(Locators.titleDialog)).getText(), "Delete Confirmation", "Title dialog Delete does not match");
+        softAssert.assertEquals(driver.findElement(By.xpath(Locators.textDialog)).getText(), "Are you sure to delete this?", "Text dialog does not match");
+        softAssert.assertEquals(driver.findElement(By.xpath(Locators.buttonDeleteDialog)).getText(), "Delete", "Button Delete on dialog does not match");
+        softAssert.assertEquals(driver.findElement(By.xpath(Locators.buttonCancelDialog)).getText(), "Cancel", "Button Cancel on dialog does not match");
+        driver.findElement(By.xpath(Locators.buttonCancelDialog)).click();
 
-
+        Thread.sleep(2000);
+        WebElement optionsEdit = driver.findElement(By.xpath(Locators.buttonEdit));
+        softAssert.assertEquals(optionsEdit.getAttribute("title"), "Edit", "Button Edit does not match");
+        Thread.sleep(2000);
         softAssert.assertAll();
+    }
+
+    @Test
+    public void itemMyBear2() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        driver.findElement(By.xpath(Locators.menuProducts)).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(Locators.menuCategory)).click();
+        Thread.sleep(1000);
+        Assert.assertEquals(driver.findElement(By.xpath(Locators.headerCategoryPage)).getText(), "All categories", "Header does not match");
+        driver.findElement(By.xpath(Locators.inputSearchCategory)).sendKeys("My bear 2");
+        driver.findElement(By.xpath(Locators.inputSearchCategory)).submit();
+        Thread.sleep(1000); //submit gây ra việc reload lại trang => cần thêm time để load
+        System.out.println("Item: " + driver.findElement(By.xpath(Locators.itemMyBear2)).getText());
     }
 
     @Test
--- src/test/java/Common/Locators.java
+++ src/test/java/Common/Locators.java
@@ -20,22 +20,31 @@ public class Locators {
     public static String menuProducts = "//span[normalize-space()='Products']";
     public static String menuPreorder = "//span[normalize-space()='Preorder']";
     public static String menuNotes = "//span[normalize-space()='Notes']";
-    public static String menuCategory = "//ul[@id='search-menu']//span[normalize-space()='Category']";
+    public static String menuCategory = "//span[normalize-space()='Category']";
 
-    //Category //h5[normalize-space()='Categories']
+    //Category
     public static String headerCategoryPage = "//h1[normalize-space()='All categories']";
     public static String headerTableCategories = "//h5[normalize-space()='Categories']";
     public static String buttonAddNewCategory = "//span[normalize-space()='Add New category']";
     public static String inputSearchCategory = "//input[@id='search']";
     public static String itemMyBear = "//td[normalize-space()='My bear']";
-    public static String toggleFeaturedFirst = "(//label[@class='aiz-switch aiz-switch-success mb-0']/input[@onchange='update_featured(this)'])[1]";
-    public static String toggleHotCategory= "(//label[@class='aiz-switch aiz-switch-success mb-0']/input[@onchange='update_hot(this)'])[1]";
+    public static String toggleFeatured = "//td[normalize-space()='My bear' and @class='d-flex align-items-center']/following-sibling::td[7]//input";
+    public static String toggleFeaturedClick = "//td[normalize-space()='My bear' and @class='d-flex align-items-center']/following-sibling::td[7]/label";
+    public static String dialogFeatured = "//span[normalize-space()='Featured categories updated successfully']";
+    public static String toggleHotCategory= "//td[normalize-space()='My bear' and @class='d-flex align-items-center']/following-sibling::td[8]//input";
+    public static String toggleHotCategoryClick= "//td[normalize-space()='My bear' and @class='d-flex align-items-center']/following-sibling::td[8]/label";
+    public static String dialogHotCategory = "//span[normalize-space()='Hot categories updated successfully']";
     public static String buttonDelete= "//a[@title='Delete'][1]";
     public static String buttonEdit= "//a[@title='Edit'][1]";
+
     public static String titleDialog= "//h4[normalize-space()='Delete Confirmation']";
     public static String buttonCancelDialog= "//button[normalize-space()='Cancel']";
     public static String buttonDeleteDialog= "//a[normalize-space()='Delete']";
     public static String textDialog= "//p[normalize-space()='Are you sure to delete this?']";
+
+    public static String itemMyBear2 = "//td[normalize-space()='My Bear 2']";
+
+
 
     //Add New Category
     public static String headerAddNewCategory = "//h5[normalize-space()='Category Information']";
